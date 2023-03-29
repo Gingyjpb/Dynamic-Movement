2d1b7f842e4dc29bc8cc0c0eda31970ee5cd5d77
JPBnZBB BARNES <jacobbarnes2201@gmail.com>
2023-03-29 13:09:42 +0100

    Second Order Constants

--- Assets/Scripts/DynamicMovement.cs
+++ Assets/Scripts/DynamicMovement.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class DynamicMovement : MonoBehaviour
 {
-    LinearSphere linearSphere;
+    public GameObject linearSphere;
 
+    private Vector3 linearPreviousPosition;
+    private Vector3 linearCurrentPosition;
     private Vector3 linearVelocity;
 
-    // Start is called before the first frame update
-    void Start()
+    private float constant1, constant2, constant3;
+
+    public DynamicMovement(float f, float zeta, float r)
     {
+        linearCurrentPosition = linearSphere.GetComponent<Transform>().position;
         
+        constant1 = zeta / (Mathf.PI * f);
+        constant2 = 1 / Mathf.Pow((2 * Mathf.PI * f), 2);
+        constant3 = r * zeta / (2 * Mathf.PI * f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        linearVelocity = (linearSphere.spherePosition - linearSphere.previousPosition) / Time.deltaTime;
+        linearPreviousPosition = linearCurrentPosition;
+        linearCurrentPosition = linearSphere.GetComponent<Transform>().position;
+
+        linearVelocity = (linearCurrentPosition - linearPreviousPosition) / Time.deltaTime;
     }
 }
