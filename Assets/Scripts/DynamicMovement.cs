7f5a0f3d428ba8e0d0dbbff404578a194c2510ee
JPBnZBB BARNES <jacobbarnes2201@gmail.com>
2023-03-29 18:54:16 +0100

    Dynamic Movement (x and y)

--- Assets/Scripts/DynamicMovement.cs
+++ Assets/Scripts/DynamicMovement.cs
@@ -6,19 +6,23 @@ public class DynamicMovement : MonoBehaviour
 {
     public GameObject linearSphere;
 
-    private Vector3 linearPreviousPosition;
-    private Vector3 linearCurrentPosition;
-    private Vector3 linearVelocity;
+    private Vector3 linearPreviousPosition, linearCurrentPosition, linearVelocity;
 
+    private Vector3 dynamicPosition, dynamicVelocity;
+
+    
+    public float f, zeta, r;
     private float constant1, constant2, constant3;
 
-    public DynamicMovement(float f, float zeta, float r)
-    {
-        linearCurrentPosition = linearSphere.GetComponent<Transform>().position;
-        
+    void Start()
+    {   
         constant1 = zeta / (Mathf.PI * f);
         constant2 = 1 / Mathf.Pow((2 * Mathf.PI * f), 2);
         constant3 = r * zeta / (2 * Mathf.PI * f);
+
+        linearCurrentPosition = linearSphere.GetComponent<Transform>().position;
+        dynamicPosition = linearSphere.GetComponent<Transform>().position;
+        dynamicVelocity = Vector3.zero;
     }
 
     // Update is called once per frame
@@ -27,6 +31,14 @@ public class DynamicMovement : MonoBehaviour
         linearPreviousPosition = linearCurrentPosition;
         linearCurrentPosition = linearSphere.GetComponent<Transform>().position;
 
-        linearVelocity = (linearCurrentPosition - linearPreviousPosition) / Time.deltaTime;
+        if (linearVelocity == Vector3.zero)
+        { 
+            linearVelocity = (linearCurrentPosition - linearPreviousPosition) / Time.deltaTime;
+        }
+
+        dynamicPosition += dynamicVelocity * Time.deltaTime;
+        dynamicVelocity += Time.deltaTime * (linearCurrentPosition + constant3 * linearVelocity - dynamicPosition - constant1 * dynamicVelocity) / constant2;
+
+        transform.position = dynamicPosition;
     }
 }
